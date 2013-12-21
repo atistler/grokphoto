7320b2e2acf724a1362f66963087b97e0bc5e311
Adam Tistler <atistler@datapipe.com>
2013-12-21 16:17:11 -0500

    deploy

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module Grokphoto
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false  # Heroku compatibility
+    config.assets.initialize_on_precompile = true  # Heroku compatibility
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     #config.assets.paths << File.join(Rails.root, 'vendor', 'assets', 'images')
